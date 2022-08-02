112dbb4de464425afe57b475f2d2bc2d3ff119e6
letev2 <roheemlateef.3@gmail.com>
2022-08-02 13:40:41 +0100

    My BankApp Project

---
+++ BankApps/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4_OOP_class
+{
+    class BankAccount
+    {
+        private string AccountNumber;
+        private string AccountName;
+        private DateTime DateCreated;
+        public double AccountBalance;
+
+      
+
+        List<string> History = new List<string>();
+
+        public BankAccount(string accountnumber, string accountname, DateTime dateCreated)
+        {
+            AccountNumber = accountnumber;
+            AccountName = accountname;
+            Deposit(10000);
+            DateCreated = dateCreated;
+        }
+
+        public void Deposit(double deposit )
+        {
+            
+            AccountBalance = AccountBalance + deposit;
+            string a = $"you deposit {deposit} and balance is {AccountBalance}";
+            History.Add(a);
+            Console.WriteLine(a);
+        }
+
+        public void WithDraw(double withdraw)
+        {
+           
+            if (withdraw <= AccountBalance)
+            {
+                AccountBalance = AccountBalance - withdraw;
+                string balance = $"your balance is {AccountBalance}"; 
+                History.Add(balance);
+                
+                Console.WriteLine($"{AccountName} you withdraw {balance} at {DateTime.Now} ");
+            }
+
+            else
+            {
+                Console.WriteLine("Insulfficient balance");
+            }
+
+        }
+
+        public void CheckAccountBalance()
+        {
+            Console.WriteLine($"Your account balance is {AccountBalance} ");
+        }
+
+        public void TransactionHistory()
+        {
+            foreach (var item in History)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ BankApps/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question_4_OOP_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BankAccount account = new BankAccount("312793751","Abdullateef", DateTime.Now);
+            BankAccount account1 = new BankAccount("312793751", "Owolabi", DateTime.Now);
+
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Enter a number between 1 to 5 to perform this operation ");
+                Console.WriteLine("Enter  1 to deposit\nEnter 2 to withdraw\nEnter 3 to check balance account\nEnter 4 to transaction History\n Enter 5 to Stop :");
+                int respond = int.Parse(Console.ReadLine());
+
+                if (respond == 1)
+                {
+                    Console.WriteLine($"Enter amount to deposit ");
+                    double deposit = double.Parse(Console.ReadLine());
+                    account.Deposit(deposit);
+                    
+                }
+
+                else if(respond == 2)
+                {
+                    Console.WriteLine($"Enter amount to Withdraw ");
+                    double withdraw = double.Parse(Console.ReadLine());
+                    account.WithDraw(withdraw);
+                    
+                }
+
+                else if (respond == 3)
+                {
+                    account.CheckAccountBalance();
+                    
+                }
+
+                else if(respond==4)
+                {
+                    account.TransactionHistory();
+                 
+                }
+
+                else if (respond==5)
+                {
+                    check = false;
+                }
+
+            }
+        }    
+    }
+
+}
